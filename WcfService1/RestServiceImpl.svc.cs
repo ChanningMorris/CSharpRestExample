61d8eea1799201155a949fce72c79505b4b9de6f
Channing Morris <chan@mefightclub.com>
2017-06-05 15:04:33 -0500

    Initial commit.

---
+++ WcfService1/RestServiceImpl.svc.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using TestNamespace;
+
+namespace WcfService1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "RestServiceImpl" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select RestServiceImpl.svc or RestServiceImpl.svc.cs at the Solution Explorer and start debugging.
+    public class RestServiceImpl : IRestServiceImpl
+    {
+        #region IRestService Members
+        public string JSONData(string id)
+        {
+            return TestClass.testMethod();
+        }
+
+        public string XMLData(string id)
+        {
+            return "You requested product " + id;
+        }
+        #endregion
+    }
+}
---
+++ WcfService1/TestClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TestMathLibrary;
+
+namespace TestNamespace
+{
+    public class TestClass
+    {
+        public static string testMethod()
+        {
+            return Class1.addSeven(1).ToString();
+        }
+    }
+}
